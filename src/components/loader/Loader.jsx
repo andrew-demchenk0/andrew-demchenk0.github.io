7009f81398baad8be840b9e8d30f8cd5d5a4efe8
Andrii Demchenko <2andrew.demchenko@gmail.com>
2023-09-07 14:51:46 +0300

    + loader component

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,16 @@
+import './loader.scss';
+
+const Loader = () => {
+  return (
+    <div className="spinner">
+      <div>AD</div>
+      <div>AD</div>
+      <div>AD</div>
+      <div>AD</div>
+      <div>AD</div>
+      <div>AD</div>
+    </div>
+  );
+};
+
+export default Loader;
