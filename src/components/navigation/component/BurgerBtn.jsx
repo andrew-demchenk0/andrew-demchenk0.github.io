7c6b469cfed86e757cfee3472288820f9718f3db
Andrii Demchenko <2andrew.demchenko@gmail.com>
2023-09-20 14:34:01 +0300

    + proptypes in burger btn

--- src/components/navigation/component/BurgerBtn.jsx
+++ src/components/navigation/component/BurgerBtn.jsx
@@ -1,14 +1,21 @@
+import PropTypes from 'prop-types';
+
 import './burgerBtn.scss';
 
 const BurgerBtn = ({ isChecked, onClick }) => {
   return (
     <>
-      <input className="burger-btn" type="checkbox" id="burger-btn" checked={isChecked} />
+      <input className="burger-btn" type="checkbox" id="burger-btn" readOnly checked={isChecked} />
       <label className="burger-btn__icon" htmlFor="burger-btn" onClick={onClick}>
         <span></span>
       </label>
     </>
   );
 };
+
+BurgerBtn.propTypes = {
+  isChecked: PropTypes.bool,
+  onClick: PropTypes.func,
+}
 
 export default BurgerBtn;
