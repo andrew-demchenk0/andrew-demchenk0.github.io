4746b5d83be8a043a51c61ab8c9f902405cd3844
Andrii Demchenko <2andrew.demchenko@gmail.com>
2023-09-20 14:49:26 +0300

    * footer code refactoring

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,38 +1,38 @@
+import { useTranslation } from 'react-i18next';
 import Logo from '../../assets/icons/logo/Logo.jsx';
 import Navigation from 'components/navigation/Navigation.jsx';
 import { FaReact } from 'react-icons/fa';
-import { useTranslation } from 'react-i18next';
 import Socials from 'components/socials/Socials.jsx';
 
-
 import './footer.scss';
+
 const Footer = () => {
   const { t } = useTranslation('global');
   return (
-    <footer className='footer'>
-      <div className='footer__nav'>
-        <Logo color="var(--title-color)" style={{ height: '30px' }} />
+    <footer className="footer">
+      <div className="footer__nav">
+        <Logo color="var(--title-color)" style={{ height: '30px' }}/>
         <Navigation className={'navigation'}/>
       </div>
-      <div className='socials'>
-        <Socials />
+      <div className="socials">
+        <Socials/>
       </div>
       <div className="footer__info">
         <div className="footer__info-item">
           {t('footer.copyright')}
-          <a href='https://www.linkedin.com/in/andrii-demchenko-21334125a/'
-             target='_blank'
+          <a href="https://www.linkedin.com/in/andrii-demchenko-21334125a/"
+             target="_blank"
              rel="noreferrer">
             {t('footer.author')}
           </a>
         </div>
         <div className="footer__info-item">
           {t('footer.create')}
-          <a href='https://react.dev/'
-             target='_blank'
+          <a href="https://react.dev/"
+             target="_blank"
              rel="noreferrer">
             React
-            <FaReact className='react-icon' color='#61DBFB'/>
+            <FaReact className="react-icon" color="#61DBFB"/>
           </a>
         </div>
       </div>
