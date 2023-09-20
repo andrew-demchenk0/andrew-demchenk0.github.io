9b96d1cc383386c2c65f866061fefe53a0e21875
Andrii Demchenko <2andrew.demchenko@gmail.com>
2023-09-20 14:47:13 +0300

    * header code refactoring

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,15 @@
+import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import Logo from '../../assets/icons/logo/Logo.jsx';
 import Navigation from 'components/navigation/Navigation.jsx';
 import ThemeSwitcher from 'components/switchers/ThemeSwitcher.jsx';
 import LangSwitcher from 'components/switchers/LangSwitcher.jsx';
+import BurgerBtn from 'components/navigation/component/BurgerBtn.jsx';
 
 import './header.scss';
-import { useMediaQuery } from 'react-responsive';
-import BurgerBtn from 'components/navigation/component/BurgerBtn.jsx';
-import { useState } from 'react';
 
 const Header = () => {
-  const isMobile = useMediaQuery({maxWidth: 768});
+  const isMobile = useMediaQuery({ maxWidth: 768 });
   const [isBurgerChecked, setIsBurgerChecked] = useState(false);
 
   const handleBurgerClick = () => {
@@ -18,27 +18,27 @@ const Header = () => {
 
   return (
     <header className={!isMobile ? 'header' : 'header header-mobile'}>
-      <Logo color="var(--title-color)" />
+      <Logo color="var(--title-color)"/>
       {!isMobile ? (
         <>
-          <Navigation className={'navigation'} />
-          <div className='switcher__wrapper'>
-            <ThemeSwitcher />
-            <LangSwitcher />
+          <Navigation className={'navigation'}/>
+          <div className="switcher__wrapper">
+            <ThemeSwitcher/>
+            <LangSwitcher/>
           </div>
         </>
       ) : (
         <>
-          <BurgerBtn isChecked={isBurgerChecked} onClick={handleBurgerClick} />
+          <BurgerBtn isChecked={isBurgerChecked} onClick={handleBurgerClick}/>
           <div className={isBurgerChecked ? 'navigation__mobile-wrapper open ' : 'navigation__mobile-wrapper'}>
-            <Navigation className={'navigation__mobile'} />
-            <div className='switcher__wrapper switcher__wrapper-mobile'>
-              <ThemeSwitcher />
-              <LangSwitcher />
+            <Navigation className={'navigation__mobile'}/>
+            <div className="switcher__wrapper switcher__wrapper-mobile">
+              <ThemeSwitcher/>
+              <LangSwitcher/>
             </div>
           </div>
         </>
-        )}
+      )}
     </header>
   );
 };
